d2480fb4436ebc58502b35700749fab1dd777f21
April <april.darger@aggiemail.usu.edu>
2015-09-08 14:28:51 -0600

    NMDS R Code

---
+++ NMDS1.R
@@ -0,0 +1,135 @@
+#install.packages("vegan")
+library(vegan)
+library(MASS)
+library(colorspace)
+# pal <- choose_palette()
+
+#---NMDS Script for Beef Basin Data --------
+# this code has been modified from Plant Community Ecology at USU and various online sources
+#### Took the file F:/LPI/LPIRelativeCover.csv and used
+#### SUM and COUNTIF in Excel to find the column sums and 
+#### number of sites each veg was found at then I manually
+#### removed those with COUNTIF below 5 AND SUMS below .05 (had to both be below to be removed)
+#### Also cleaned up the data (combined SALS0 and SALSO, etc...)
+
+data<-read.csv("F:/LPI/AprilLPIRelativeCoverCommonInExcel.csv",header=TRUE, row.names=1)
+###data<-data[,2:ncol(data)] 
+
+# calculate dissimilarities, use function "vegdist"in VEGAN package
+data.dis<-vegdist(data,method="bray")
+dis.matrix<-as.matrix(data.dis)
+# rankindex compares euclidean, bray-curtis, etc... for my data
+rankindex(dis.matrix,data)
+
+
+data.mds<-metaMDS(comm=data,distance="bray",trace=FALSE)
+data.mds
+# Stress <0.10 indicates that the ordination is good "with no real 
+# risk of drawing false inferences" (Clarke 1993, p. 26). 
+# linear fit is the fit between ordination values and distances
+stressplot(data.mds) 
+
+###########################  ENVIRONMENTAL DATA  ###########################################
+
+
+# read in Soil environmental data
+data.env <- read.csv("F:/Soils/SoilEnvironmentalDataModWithColbyAWS.csv",header=TRUE, row.names=1)
+###data.env<-data.env[,2:ncol(data.env)]
+
+# read in Artr live and dead as environmental data
+data.l <- read.csv("F:/ShrubDensity/HeightClass/LivePlotbySizeClass.csv", header=TRUE, row.names=1)
+data.d <- read.csv("F:/ShrubDensity/HeightClass/DeadPlotbySizeClass.csv", header=TRUE, row.names=1)
+rel.l <- read.csv("F:/ShrubDensity/HeightClass/LiveSizeClassPctCover.csv", header=TRUE, row.names=1)
+rel.d <- read.csv("F:/ShrubDensity/HeightClass/DeadSizeClassPctCover.csv", header=TRUE, row.names=1)
+rel.l[is.na(rel.l)] <- 0 # replace NA with 0
+rel.d[is.na(rel.d)] <- 0 # replace NA with 0
+# #add shrub height class densities as environmental data
+#   # remove columns with only 0 values where there were no shrubs of that species in that size class found.
+# A <- read.csv("F:/ShrubDensity/HeightClass/AprilAdensityM2.csv",row.names=1)
+#   A <- A[, colSums(A != 0) > 0]
+# B <- read.csv("F:/ShrubDensity/HeightClass/AprilBdensityM2.csv",row.names=1)
+#   B <- B[, colSums(B != 0) > 0]
+# C <- read.csv("F:/ShrubDensity/HeightClass/AprilCdensityM2.csv",row.names=1)
+#   C <- C[, colSums(C != 0) > 0]
+# D <- read.csv("F:/ShrubDensity/HeightClass/AprilDdensityM2.csv",row.names=1)
+#   D <- D[, colSums(D != 0) > 0]
+# E <- read.csv("F:/ShrubDensity/HeightClass/AprilEdensityM2.csv",row.names=1)
+#   E <- E[, colSums(E != 0) > 0]
+
+#function "envfit" fits environmental vectors or factors onto an ordination.
+#requires ordination plot first before plot(fit)
+fit.env <- envfit(data.mds,data.env,perm=1000)
+fit.l <- envfit(data.mds,data.l,perm=1000)
+fit.d <- envfit(data.mds,data.d,perm=1000)
+fit.rl <- envfit(data.mds,rel.l,perm=1000)
+fit.rd <- envfit(data.mds,rel.d,perm=1000)
+# fit.a<-envfit(data.mds,A,perm=1000)
+# fit.b<-envfit(data.mds,B,perm=1000)
+# fit.c<-envfit(data.mds,C,perm=1000)
+# fit.d<-envfit(data.mds,D,perm=1000)
+# fit.e<-envfit(data.mds,E,perm=1000)
+# Look at the significant factors
+fit.env
+fit.l
+fit.d
+fit.rl
+fit.rd
+# fit.a
+# fit.b
+# fit.c
+# fit.d
+# fit.e
+### Choose only the significant environmental data
+sig.data.env<-data.env[,c(1,7:9,19:25,28:29)]
+sig.fit.env<-envfit(data.mds,sig.data.env,perm=1000)
+sig.fit.env # Check that you pulled up the right factors.
+
+# sig.A<-A[,c(1,3)]
+# sig.fit.A<-envfit(data.mds,sig.A,perm=1000)
+# sig.fit.A # Check that you pulled up the right factors.
+# 
+# sig.B<-B[,c(2:3,9)]
+# sig.fit.B<-envfit(data.mds,sig.B,perm=1000)
+# sig.fit.B # Check that you pulled up the right factors.
+# 
+# sig.C<-C[,c(1:3,6,8,12:13)]
+# sig.fit.C<-envfit(data.mds,sig.C,perm=1000)
+# sig.fit.C # Check that you pulled up the right factors.
+# 
+# sig.D<-D[,c(3,8,11,13)]
+# sig.fit.D<-envfit(data.mds,sig.D,perm=1000)
+# sig.fit.D # Check that you pulled up the right factors.
+# 
+# sig.E<-E[,c(3,7:8)]
+# sig.fit.E<-envfit(data.mds,sig.E,perm=1000)
+# sig.fit.E # Check that you pulled up the right factors.
+
+
+#plotMDS
+
+#ordiplot(data.mds)
+ordiplot(data.mds, display ="species", type ="n")
+text(data.mds, display="sites", col="black", cex=0.7)
+text(data.mds, display="species", col="red", cex=0.7)
+
+#plot environmental loadings
+plot(data.mds,type="t",main="NMDS using Euclidean Distance, Soil Data")
+  plot(sig.fit.env,col="blue", cex=0.7)
+plot(data.mds,type="t",main="NMDS using Euclidean Distance, ARTR.L Data")
+  plot(fit.l,col="green", cex=0.7)
+plot(data.mds,type="t",main="NMDS using Euclidean Distance, ARTR.D Data")
+  plot(fit.d,col="purple", cex=0.7)
+plot(data.mds,type="t",main="NMDS using Euclidean Distance, % ARTR.L Data")
+  plot(fit.rl,col="yellow", cex=0.7)
+plot(data.mds,type="t",main="NMDS using Euclidean Distance, % ARTR.D Data")
+  plot(fit.rd,col="orange", cex=0.7)
+# plot(data.mds,type="t",main="NMDS using Euclidean Distance, Shrubs <15cm Juvenile ")
+#   plot(sig.fit.A,col="orange", cex=0.7)
+# plot(data.mds,type="t",main="NMDS using Euclidean Distance, Shrubs <15cm Mature")
+#   plot(sig.fit.B,col="purple", cex=0.7)
+# plot(data.mds,type="t",main="NMDS using Euclidean Distance, Shrubs 15-50cm")
+#   plot(sig.fit.C,col="green", cex=0.7)
+# plot(data.mds,type="t",main="NMDS using Euclidean Distance, Shrubs >50c-100cm")
+#   plot(sig.fit.D,col="turquoi3se2", cex=0.7)
+# plot(data.mds,type="t",main="NMDS using Euclidean Distance, Shrubs >100cm")
+#   plot(sig.fit.E,col="hotpink", cex=0.7)

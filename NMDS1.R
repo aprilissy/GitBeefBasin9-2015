b036f0763d39903d9815fa442d2621b6c0e96a64
April <april.darger@aggiemail.usu.edu>
2015-09-11 18:26:40 -0600

    changed bray to euc throughout code, added sageNMDS throughout code

--- NMDS1.R
+++ NMDS1.R
@@ -48,6 +48,7 @@ data.env <- read.csv("F:/Soils/SoilEnvironmentalDataModWithColbyAWS.csv",header=
 
 # read in SageEnvironmentalNMDS data
 data.sage <- read.csv("F:/SageNMDSvariables/Sage.Env.April.csv",header=TRUE,row.names=1)
+data.sage[is.na(data.sage)] <- 0 # replace NA with 0
 
 # read in Artr live and dead as environmental data
 data.l <- read.csv("F:/ShrubDensity/HeightClass/LivePlotbySizeClass.csv", header=TRUE, row.names=1)
@@ -56,6 +57,7 @@ rel.l.sage <- read.csv("F:/ShrubDensity/HeightClass/LiveSizeClassSagePctCover.cs
 rel.d.sage <- read.csv("F:/ShrubDensity/HeightClass/DeadSizeClassSagePctCover.csv", header=TRUE, row.names=1)
 rel.l.total <- read.csv("F:/ShrubDensity/HeightClass/LiveSizeClassTotalPctCover.csv", header=TRUE, row.names=1)
 rel.d.total <- read.csv("F:/ShrubDensity/HeightClass/DeadSizeClassTotalPctCover.csv", header=TRUE, row.names=1)
+
 rel.l.sage[is.na(rel.l.sage)] <- 0 # replace NA with 0
 rel.d.sage[is.na(rel.d.sage)] <- 0 # replace NA with 0
 rel.l.total[is.na(rel.l.total)] <- 0 # replace NA with 0
@@ -63,15 +65,17 @@ rel.d.total[is.na(rel.d.total)] <- 0 # replace NA with 0
 
 #function "envfit" fits environmental vectors or factors onto an ordination.
 #requires ordination plot first before plot(fit)
-fit.env <- envfit(data.mds,data.env,perm=1000)
-fit.l <- envfit(data.mds,data.l,perm=1000)
-fit.d <- envfit(data.mds,data.d,perm=1000)
-fit.rls <- envfit(data.mds,rel.l.sage,perm=1000)
-fit.rds <- envfit(data.mds,rel.d.sage,perm=1000)
-fit.rlt <- envfit(data.mds,rel.l.total,perm=1000)
-fit.rdt <- envfit(data.mds,rel.d.total,perm=1000)
+fit.env <- envfit(e.data.mds,data.env,perm=1000)
+fit.sage <- envfit(e.data.mds, data.sage,perm=1000)
+fit.l <- envfit(e.data.mds,data.l,perm=1000)
+fit.d <- envfit(e.data.mds,data.d,perm=1000)
+fit.rls <- envfit(e.data.mds,rel.l.sage,perm=1000)
+fit.rds <- envfit(e.data.mds,rel.d.sage,perm=1000)
+fit.rlt <- envfit(e.data.mds,rel.l.total,perm=1000)
+fit.rdt <- envfit(e.data.mds,rel.d.total,perm=1000)
 
 fit.env
+fit.sage
 fit.l
 fit.d
 fit.rls
@@ -80,29 +84,31 @@ fit.rlt
 fit.rdt
 
 ### Choose only the significant environmental data
-sig.data.env<-data.env[,c(1,7:9,19:25,28:29)]
-sig.fit.env<-envfit(data.mds,sig.data.env,perm=1000)
+sig.data.env<-data.env[,c(8:9,12,16,17,24)]
+sig.fit.env<-envfit(e.data.mds,sig.data.env,perm=1000)
 sig.fit.env # Check that you pulled up the right factors.
 
 #plotMDS
 
 #ordiplot(data.mds)
-ordiplot(data.mds, display ="species", type ="n")
-text(data.mds, display="sites", col="black", cex=0.7)
-text(data.mds, display="species", col="red", cex=0.7)
+ordiplot(e.data.mds, display ="species", type ="n")
+text(e.data.mds, display="sites", col="black", cex=0.7)
+text(e.data.mds, display="species", col="red", cex=0.7)
 
 #plot environmental loadings
-plot(data.mds,type="t",main="NMDS Soil Data")
+plot(e.data.mds,type="t",main="NMDS Soil Data")
   plot(sig.fit.env,col="blue", cex=0.7)
-plot(data.mds,type="t",main="NMDS ARTR.L Data")
-  plot(fit.l,col="green", cex=0.7)
-plot(data.mds,type="t",main="NMDS ARTR.D Data")
+plot(e.data.mds,type="t",main="NMDS Sage Data")
+  plot(fit.sage,col="chartreuse4", cex=0.7)
+plot(e.data.mds,type="t",main="NMDS ARTR.L Data")
+  plot(fit.l,col="green3", cex=0.7)
+plot(e.data.mds,type="t",main="NMDS ARTR.D Data")
   plot(fit.d,col="purple", cex=0.7)
-plot(data.mds,type="t",main="NMDS % ARTR.L/All ARTR Data")
+plot(e.data.mds,type="t",main="NMDS % ARTR.L/All ARTR Data")
   plot(fit.rls,col="yellow", cex=0.7)
-plot(data.mds,type="t",main="NMDS % ARTR.D/All ARTR Data")
+plot(e.data.mds,type="t",main="NMDS % ARTR.D/All ARTR Data")
   plot(fit.rds,col="orange", cex=0.7)
-plot(data.mds,type="t",main="NMDS % ARTR.L/All Shrubs Data")
+plot(e.data.mds,type="t",main="NMDS % ARTR.L/All Shrubs Data")
   plot(fit.rlt,col="brown", cex=0.7)
-plot(data.mds,type="t",main="NMDS % ARTR.D/All Shrubs Data")
+plot(e.data.mds,type="t",main="NMDS % ARTR.D/All Shrubs Data")
   plot(fit.rdt,col="sienna", cex=0.7)

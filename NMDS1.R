3a13728e9af73350cff707dec10cc05a1092d53a
April <april.darger@aggiemail.usu.edu>
2015-09-14 19:19:42 -0600

    added class density throughout R code

--- NMDS1.R
+++ NMDS1.R
@@ -53,14 +53,17 @@ data.sage[is.na(data.sage)] <- 0 # replace NA with 0
 data.l <- read.csv("F:/ShrubDensity/HeightClass/LivePlotbySizeClass.csv", header=TRUE, row.names=1)
 data.d <- read.csv("F:/ShrubDensity/HeightClass/DeadPlotbySizeClass.csv", header=TRUE, row.names=1)
 data.l.d <-read.csv("F:/ShrubDensity/HeightClass/LiveDeadPlotbySizeClass.csv", header=TRUE, row.names=1) 
-# rel.l.sage <- read.csv("F:/ShrubDensity/HeightClass/LiveSizeClassSagePctCover.csv", header=TRUE, row.names=1)
+prop.l.sage <- read.csv("F:/ShrubDensity/HeightClass/LiveSizeClassSagePctCover.csv", header=TRUE, row.names=1)
 # rel.d.sage <- read.csv("F:/ShrubDensity/HeightClass/DeadSizeClassSagePctCover.csv", header=TRUE, row.names=1)
 # rel.l.d.sage <- read.csv("F:/ShrubDensity/HeightClass/LiveDeadSizeClassSagePctCover.csv", header=TRUE, row.names=1)
 rel.l.total <- read.csv("F:/ShrubDensity/HeightClass/LiveSizeClassTotalPctCover.csv", header=TRUE, row.names=1)
 rel.d.total <- read.csv("F:/ShrubDensity/HeightClass/DeadSizeClassTotalPctCover.csv", header=TRUE, row.names=1)
 rel.l.d.total <- read.csv("F:/ShrubDensity/HeightClass/LiveDeadSizeClassTotalPctCover.csv", header=TRUE, row.names=1)
+den.l <- read.csv("F:/ShrubDensity/HeightClass/LiveDensityM2Class.csv", header=TRUE, row.names=1)
+den.d <- read.csv("F:/ShrubDensity/HeightClass/DeadDensityM2Class.csv", header=TRUE, row.names=1)
+den.l.d <- read.csv("F:/ShrubDensity/HeightClass/LiveDeadDensityM2Class.csv", header=TRUE, row.names=1)
 
-# rel.l.sage[is.na(rel.l.sage)] <- 0 # replace NA with 0
+prop.l.sage[is.na(prop.l.sage)] <- 0 # replace NA with 0
 # rel.d.sage[is.na(rel.d.sage)] <- 0 # replace NA with 0
 # rel.l.d.sage[is.na(rel.l.d.sage)] <- 0 # replace NA with 0
 rel.l.total[is.na(rel.l.total)] <- 0 # replace NA with 0
@@ -74,24 +77,29 @@ fit.sage <- envfit(e.data.mds, data.sage,perm=1000)
 fit.l <- envfit(e.data.mds,data.l,perm=1000)
 fit.d <- envfit(e.data.mds,data.d,perm=1000)
 fit.l.d <- envfit(e.data.mds,data.l.d,perm=1000)
-# fit.rls <- envfit(e.data.mds,rel.l.sage,perm=1000)
+fit.pls <- envfit(e.data.mds,prop.l.sage,perm=1000)
 # fit.rds <- envfit(e.data.mds,rel.d.sage,perm=1000)
 # fit.rlds <- envfit(e.data.mds,rel.l.d.sage,perm=1000)
 fit.rlt <- envfit(e.data.mds,rel.l.total,perm=1000)
 fit.rdt <- envfit(e.data.mds,rel.d.total,perm=1000)
 fit.rldt <- envfit(e.data.mds,rel.l.d.total,perm=1000)
+fit.den.l <- envfit(e.data.mds,den.l.total,perm=1000)
+fit.den.d <- envfit(e.data.mds,den.d.total,perm=1000)
+fit.den.l.d <- envfit(e.data.mds,den.l.d.total,perm=1000)
 
 fit.env
 fit.sage
 fit.l
 fit.d
 fit.l.d
-# fit.rls
+fit.pls
 # fit.rds
 # fit.rlds
 fit.rlt
 fit.rdt
 fit.rldt
+fit.den.l
+fit.den.l.d
 
 ### Choose only the significant environmental data
 sig.data.env<-data.env[,c(8,9,12,16:18,24)]
@@ -102,24 +110,36 @@ sig.fit.env # Check that you pulled up the right factors.
 #plotMDS
 orditkplot(e.data.mds, display="species", col="black", cex=0.7, pcol="gray",pch="+",xlim=c(-0.7,0.7),ylim=c(-0.7,0.4))
 saveRDS(Plot, file="F:/SageNMDSvariables/Plot.Rdata")
-readRDS("F:/SageNMDSvariables/Plot.Rdata")
-plot(Plot)
-title(main = "NMDS Sagebrush Vectors")
+Plot <- readRDS("F:/SageNMDSvariables/Plot.Rdata")
 
+plot(Plot)
+title(main = "NMDS and Soils")
 plot(sig.fit.env,col="blue", cex=0.7,font=2)
+
+plot(Plot)
+title(main = "NMDS and Sagebrush")
 plot(fit.sage,col="chartreuse4", cex=0.7,font=2)
 
+plot(Plot)
+title(main = "NMDS and Live Classes")
 plot(fit.l,col="blue", cex=0.7,font=2)
-# plot(fit.rls,col="dodgerblue", cex=0.7,font=2)
-plot(fit.rlt,col="dodgerblue", cex=0.7,font=2)
+plot(fit.pls,col="dodgerblue", cex=0.7,font=2)
+plot(fit.rlt,col="blue4", cex=0.7,font=2)
+plot(fit.den.l,col="blue4", cex=0.7,font=2)
 
+plot(Plot)
+title(main = "NMDS and Dead Classes")
 plot(fit.d,col="blue", cex=0.7,font=2)
 # plot(fit.rds,col="orange", cex=0.7,font=2)
 plot(fit.rdt,col="pink4", cex=0.7,font=2)
+plot(fit.den.d,col="blue4", cex=0.7,font=2)
 
+plot(Plot)
+title(main = "NMDS and Live+Dead Classes")
 plot(fit.l.d,col="blue", cex=0.7,font=2)
 # plot(fit.rlds,col="chartreuse4", cex=0.7,font=2)
 plot(fit.rldt,col="blue", cex=0.7,font=2)
+plot(fit.den.l.d,col="blue4", cex=0.7,font=2)
 
 #ordiplot(data.mds)
 # ordiplot(e.data.mds, display ="species", type ="n")

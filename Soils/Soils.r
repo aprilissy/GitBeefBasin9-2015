5d104a65363ae11336e83fa92a38b7e6d3c81c15
April <april.darger@aggiemail.usu.edu>
2015-09-18 17:09:29 -0600

    Getting April and USGS soils up to date in R

--- Soils/Soils.r
+++ Soils/Soils.r
@@ -3,11 +3,13 @@
 library(plyr)
 library(data.table)
 library(splitstackshape)
+library(aqp)
+library(reshape2)
 
 # Read in Soils Data
 dat <- read.csv("F:/Soils/SoilDataFitUSGSColumns.csv", header = T,nrows = 444)
-dat <- subset(dat, select = -c(Horizon,Theta_fc,Theta_pwp) )
-SlopeShape <- dat[,c(1,21)] # Slope shape
+dat <- subset(dat, select = -c(Horizon,Theta_fc,Theta_pwp, HzNum) )
+SlopeShape <- dat[,c(1,20)] # Slope shape
 SlopeShape <- SlopeShape[complete.cases(SlopeShape), ] #Remove rows with only NA
 # Combine Slope Shape categories
 SlopeShape$SlopeShape <- sub("LC", "CL", SlopeShape$SlopeShape, ignore.case = FALSE)
@@ -16,6 +18,88 @@ SlopeShape$SlopeShape <- sub("VL", "LV", SlopeShape$SlopeShape, ignore.case = FA
 SlopeShape$SlopeShape <- as.factor(SlopeShape$SlopeShape)
 dat <- dat[,-21]
 
+#Look at data
+names(dat)
+sapply(dat, class)
+
+f2 <- function(x) summary(x[,-1])
+summary <- ddply( dat, .(id), function(x) summary(x[,-1]) )
+
+#AWC
+TotalAWC <- ddply(dat, 'id', summarize, TotalAWC = sum(AWHC, na.rm = T))
+MaxAWC <- ddply(dat, 'id', summarize, MaxAWC = max(AWHC, na.rm = T))
+Slope <- ddply( dat, .(id), function(x) max(x$Slope, na.rm = T) )
+  names(Slope)[2] <- 'Slope'
+maxDepth <- ddply( dat, .(id), function(x) max(x$bottom, na.rm = T) )
+  names(maxDepth)[2] <- 'maxDepth'
+maxSand <- ddply( dat, .(id), function(x) max(x$SandPercent, na.rm = T) )
+  names(maxSand)[2] <- 'maxSand'
+minSand <- ddply( dat, .(id), function(x) min(x$SandPercent, na.rm = T) )
+  names(minSand)[2] <- 'minSand'
+minClay <- ddply( dat, .(id), function(x) min(x$ClayPercent, na.rm = T) )
+  names(minClay)[2] <- 'minClay'
+maxClay <- ddply( dat, .(id), function(x) max(x$ClayPercent, na.rm = T) )
+  names(maxClay)[2] <- 'maxClay'
+Elevation <- ddply( dat, .(id), function(x) max(x$Elevation, na.rm = T) )
+  names(Elevation)[2] <- 'Elevation'
+maxpH <- ddply( dat, .(id), function(x) max(x$pH, na.rm = T) )
+  names(maxpH)[2] <- 'maxpH'
+minpH <- ddply( dat, .(id), function(x) min(x$pH, na.rm = T) )
+  names(minpH)[2] <- 'minpH'
+CarbonateStage <- ddply( dat, .(id), function(x) max(x$CarbonateStage, na.rm = T) )
+  names(CarbonateStage)[2] <- 'CarbonateStage'
+BioticCrustClass <- ddply( dat, .(id), function(x) max(x$BioticCrustClass, na.rm = T) )
+  names(BioticCrustClass)[2] <- 'BioticCrustClass'
+
+
+ #Factor
+maxDryValue <- ddply(.data = dat, .(id), function(x) max(x$DryValue, na.rm = T))
+ names(maxDryValue)[2] <- 'maxDryValue'
+minDryValue <- ddply(.data = dat, .(id), function(x) min(x$DryValue, na.rm = T))
+ names(minDryValue)[2] <- 'minDryValue'
+maxDryChroma <- ddply(.data = dat, .(id), function(x) max(x$DryChroma, na.rm = T))
+ names(maxDryChroma)[2] <- 'maxDryChroma'
+minDryChroma <- ddply(.data = dat, .(id), function(x) min(x$DryChroma, na.rm = T))
+ names(minDryChroma)[2] <- 'minDryChroma'
+maxMoistValue <- ddply(.data = dat, .(id), function(x) max(x$MoistValue, na.rm = T))
+ names(maxMoistValue)[2] <- 'maxMoistValue'
+minMoistValue <- ddply(.data = dat, .(id), function(x) min(x$MoistValue, na.rm = T))
+ names(minMoistValue)[2] <- 'minMoistValue'
+maxMoistChroma <- ddply(.data = dat, .(id), function(x) max(x$MoistChroma, na.rm = T))
+ names(maxMoistChroma)[2] <- 'maxMoistChroma'
+minMoistChroma <- ddply(.data = dat, .(id), function(x) min(x$MoistChroma, na.rm = T))
+ names(minMoistChroma)[2] <- 'minMoistChroma'
+
+# Change moist Chroma to 1st horizon and 2nd horizon as a proxy
+# for organic carbon accumulation in surface and subsurface horzons
+# A lower chroma means more organic carbon. Grassland soils have
+# higher fine root turnover.
+
+# Surface
+dat$Surface <- as.numeric(dat$top<1) # Turns surface horizon into 1, and all other into 0
+
+max.func.sur <- function(dat) {
+  max.moist.sur <- max(dat$Surface)
+  
+  return(data.frame(Surface = dat$MoistChroma[dat$Surface==max.moist.sur]))
+}
+
+Surface <- ddply(dat, .(id), max.func.sur)
+names(Surface)[2] <- 'Surface'
+
+# Subsurface
+dat <- getanID(data = dat, id.vars = "id") # Creates an ordered list of each horizon in a plot
+dat$Subsurface <- as.numeric(dat$.id==2) # Makes subsurface horizon 1, and all other horizons 0
+dat$MoistChroma[is.na(dat$MoistChroma)] <- 0
+
+max.func.sub <- function(dat) {
+  max.moist.sub <- max(dat$Subsurface)
+  
+  return(data.frame(Subsurface = dat$MoistChroma[dat$Subsurface==max.moist.sub]))
+}
+
+Subsurface <- ddply(dat, .(id), max.func.sub)
+names(Subsurface)[2] <- 'Subsurface'
 
 #Numeric Data
 dat$top <- as.numeric(dat$top)
@@ -43,106 +127,8 @@ dat$HzNum <- as.factor(dat$HzNum)
 dat$Aspect <- as.factor(dat$Aspect)
 
 
-# Create Functions for extracting to single value per unique id(plot)
-f1 <- function(x) max(x$ClayPercent, na.rm = T) 
-f111 <- function(x) min(x$ClayPercent, na.rm = T) 
-f2 <- function(x) summary(x[,-1])
-f3 <- function(x) max(x$bottom, na.rm = T)
-f4 <- function(x) max(x$SandPercent, na.rm = T)
-f41 <- function(x) min(x$SandPercent, na.rm = T)
-f5 <- function(x) max(x$Elevation, na.rm = T)
-f6 <- function(x) max(x$pH, na.rm = T)
-f7 <- function(x) min(x$pH, na.rm = T)
-f8 <- function(x) max(x$DryValue, na.rm = T)
-f9 <- function(x) min(x$DryValue, na.rm = T)
-f10 <- function(x) max(x$DryChroma, na.rm = T)
-f11 <- function(x) min(x$DryChroma, na.rm = T)
-f12 <- function(x) max(x$MoistValue, na.rm = T)
-f13 <- function(x) min(x$MoistValue, na.rm = T)
-f14 <- function(x) max(x$MoistChroma, na.rm = T)
-f15 <- function(x) min(x$MoistChroma, na.rm = T)
-f16 <- function(x) max(x$CarbonateStage, na.rm = T)
-f17 <- function(x) max(x$BioticCrustClass, na.rm = T)
-f18 <- function(x) max(x$Slope, na.rm = T)
-
-
-# pull out max/min for 1 value per unique id(plot)
- #Numeric
-maxClay <- ddply(.data = df1, .(id), .fun = f1)
- names(maxClay)[2] <- 'maxClay'
-minClay <- ddply(.data = df1, .(id), .fun = f111)
- names(minClay)[2] <- 'minClay'
-maxDepth <- ddply(.data = df1, .(id), .fun = f3)
- names(maxDepth)[2] <- 'maxDepth'
-maxSand <- ddply(.data = df1, .(id), .fun = f4)
- names(maxSand)[2] <- 'maxSand'
-minSand <- ddply(.data = df1, .(id), .fun = f41)
- names(minSand)[2] <- 'minSand'
-Elevation <- ddply(.data = df1, .(id), .fun = f5)
- names(Elevation)[2] <- 'Elevation'
-maxpH <- ddply(.data = df1, .(id), .fun = f6)
- names(maxpH)[2] <- 'maxpH'
-minpH <- ddply(.data = df1, .(id), .fun = f7)
- names(minpH)[2] <- 'minpH'
-Slope <- ddply(.data = df1, .(id), .fun = f18)
- names(Slope)[2] <- 'Slope'
-
- #Factor
-maxDryValue <- ddply(.data = df1, .(id), .fun = f8)
- names(maxDryValue)[2] <- 'maxDryValue'
-minDryValue <- ddply(.data = df1, .(id), .fun = f9)
- names(minDryValue)[2] <- 'minDryValue'
-maxDryChroma <- ddply(.data = df1, .(id), .fun = f10)
- names(maxDryChroma)[2] <- 'maxDryChroma'
-minDryChroma <- ddply(.data = df1, .(id), .fun = f11)
- names(minDryChroma)[2] <- 'minDryChroma'
-maxMoistValue <- ddply(.data = df1, .(id), .fun = f12)
- names(maxMoistValue)[2] <- 'maxMoistValue'
-minMoistValue <- ddply(.data = df1, .(id), .fun = f13)
- names(minMoistValue)[2] <- 'minMoistValue'
-maxMoistChroma <- ddply(.data = df1, .(id), .fun = f14)
- names(maxMoistChroma)[2] <- 'maxMoistChroma'
-minMoistChroma <- ddply(.data = df1, .(id), .fun = f15)
- names(minMoistChroma)[2] <- 'minMoistChroma'
-CarbonateStage <- ddply(.data = df1, .(id), .fun = f16)
- names(CarbonateStage)[2] <- 'CarbonateStage'
-BioticCrustClass <- ddply(.data = df1, .(id), .fun = f17)
- names(BioticCrustClass)[2] <- 'BioticCrustClass'
-
-
-# Change moist Chroma to 1st horizon and 2nd horizon as a proxy
-# for organic carbon accumulation in surface and subsurface horzons
-# A lower chroma means more organic carbon. Grassland soils have
-# higher fine root turnover.
-
-# Surface
-df1$Surface <- as.numeric(df1$top<1) # Turns surface horizon into 1, and all other into 0
-
-max.func.sur <- function(df1) {
-  max.moist.sur <- max(df1$Surface)
-  
-  return(data.frame(Surface = df1$MoistChroma[df1$Surface==max.moist.sur]))
-}
-
-Surface <- ddply(df1, .(id), max.func.sur)
-names(Surface)[2] <- 'Surface'
-
-# Subsurface
-df1 <- getanID(data = df1, id.vars = "id") # Creates an ordered list of each horizon in a plot
-df1$Subsurface <- as.numeric(df1$.id==2) # Makes subsurface horizon 1, and all other horizons 0
-df1$MoistChroma[is.na(df1$MoistChroma)] <- 0
-
-max.func.sub <- function(df1) {
-  max.moist.sub <- max(df1$Subsurface)
-  
-  return(data.frame(Subsurface = df1$MoistChroma[df1$Subsurface==max.moist.sub]))
-}
-
-Subsurface <- ddply(df1, .(id), max.func.sub)
-names(Subsurface)[2] <- 'Subsurface'
-
-
 all <- join(Elevation, SlopeShape, by = 'id', type = 'inner')
+all <- join(all, Slope, by = 'id', type = 'inner')
 all <- join(all, maxClay, by = 'id', type = 'inner')
 all <- join(all, minClay, by = 'id', type = 'inner')
 all <- join(all, maxSand, by = 'id', type = 'inner')
@@ -163,6 +149,7 @@ all <- join(all, BioticCrustClass, by = 'id', type = 'inner')
 all <- join(all, Surface, by = 'id', type = 'inner')
 all <- join(all, Subsurface, by = 'id', type = 'inner')
 
+
 # Create new soil parameter where depth is binary.
  # if the maximum depth is >50/100/150/200 then 1, if not then 0
 all$Depth50 <- as.numeric(all$maxDepth > 50)
@@ -170,10 +157,32 @@ all$Depth100 <- as.numeric(all$maxDepth > 100)
 all$Depth150 <- as.numeric(all$maxDepth > 150)
 all$Depth200 <- as.numeric(all$maxDepth == 200)
 
+#Now calculate depth weighted averages of each continuous variable, then append these to the other variables. 
+#Convert to SoilProfileCollection
+depths(dat) <- id ~ top + bottom
 
+# within each profile, compute weighted means, over the intervals: 0-25,0-50,0-100, removing NA if present 
+d25 <- slab(dat, id ~ AWHC, slab.structure = c(0,25), slab.fun = mean, na.rm=TRUE)
+d50 <- slab(dat, id ~ AWHC, slab.structure = c(0,50), slab.fun = mean, na.rm=TRUE)
+d100 <- slab(dat, id ~ AWHC, slab.structure = c(0,100), slab.fun = mean, na.rm=TRUE)
 
-write.csv(all,file="F:/Soils/SoilEnvironmentalData.csv")
+# reshape to wide format, remove unneeded variables and rename. 
+AWC25 <- dcast(d25, id + top + bottom ~ variable, value.var = 'value')
+AWC25 <- AWC25[,-c(2,3)]
+names(AWC25)[2] <- 'AWC25'
 
-# After all this I manually added Colby's AWS data to this file
-#   and called it SoilEnvironmentalDataModWithColbyAWS.csv
-#   Also remove first column of 12345678....
+AWC50 <- dcast(d50, id + top + bottom ~ variable, value.var = 'value')
+AWC50 <- AWC50[,-c(2,3)]
+names(AWC50)[2] <- 'AWC50'
+
+AWC100 <- dcast(d100, id + top + bottom ~ variable, value.var = 'value')
+AWC100 <- AWC100[,-c(2,3)]
+names(AWC100)[2] <- 'AWC100'
+
+all <- join(all, AWC25, by = 'id', type = 'inner')
+all <- join(all, AWC50, by = 'id', type = 'inner')
+all <- join(all, AWC100, by = 'id', type = 'inner')
+all <- join(all, MaxAWC, by = 'id', type = 'inner')
+all <- join(all, TotalAWC, by = 'id', type = 'inner')
+
+write.csv(all,file="F:/Soils/SoilEnvironmentalData.csv", row.names=FALSE)
---
+++ Soils/USGSSoils.R
@@ -0,0 +1,189 @@
+#USGS Soil udata
+#install.packages('XLConnect')
+library(plyr)
+library(data.table)
+library(splitstackshape)
+library(aqp)
+library(reshape2)
+library(XLConnect)
+
+# Read in Soils udata
+udat <- read.csv("F:/Soils/USGSsoildataModForAprilsdata.csv", header = T,nrows = 444)
+udat <- subset(udat, select = -c(Horizon,Theta_fc,Theta_pwp) )
+# SlopeShape <- udat[,c(1,21)] # Slope shape
+# SlopeShape <- SlopeShape[complete.cases(SlopeShape), ] #Remove rows with only NA
+# # Combine Slope Shape categories
+# SlopeShape$SlopeShape <- sub("LC", "CL", SlopeShape$SlopeShape, ignore.case = FALSE)
+# SlopeShape$SlopeShape <- sub("VC", "CV", SlopeShape$SlopeShape, ignore.case = FALSE)
+# SlopeShape$SlopeShape <- sub("VL", "LV", SlopeShape$SlopeShape, ignore.case = FALSE)
+# SlopeShape$SlopeShape <- as.factor(SlopeShape$SlopeShape)
+# udat <- udat[,-21]
+
+#Look at udata
+names(udat)
+sapply(udat, class)
+
+f2 <- function(x) summary(x[,-1])
+summary <- ddply( udat, .(id), function(x) summary(x[,-1]) )
+
+#AWC
+TotalAWC <- ddply(udat, 'id', summarize, TotalAWC = sum(AWHC, na.rm = T))
+MaxAWC <- ddply(udat, 'id', summarize, MaxAWC = max(AWHC, na.rm = T))
+Slope <- ddply( udat, .(id), function(x) max(x$Slope, na.rm = T) )
+names(Slope)[2] <- 'Slope'
+maxDepth <- ddply( udat, .(id), function(x) max(x$bottom, na.rm = T) )
+names(maxDepth)[2] <- 'maxDepth'
+maxSand <- ddply( udat, .(id), function(x) max(x$SandPercent, na.rm = T) )
+names(maxSand)[2] <- 'maxSand'
+minSand <- ddply( udat, .(id), function(x) min(x$SandPercent, na.rm = T) )
+names(minSand)[2] <- 'minSand'
+minClay <- ddply( udat, .(id), function(x) min(x$ClayPercent, na.rm = T) )
+names(minClay)[2] <- 'minClay'
+maxClay <- ddply( udat, .(id), function(x) max(x$ClayPercent, na.rm = T) )
+names(maxClay)[2] <- 'maxClay'
+Elevation <- ddply( udat, .(id), function(x) max(x$Elevation, na.rm = T) )
+names(Elevation)[2] <- 'Elevation'
+maxpH <- ddply( udat, .(id), function(x) max(x$pH, na.rm = T) )
+names(maxpH)[2] <- 'maxpH'
+minpH <- ddply( udat, .(id), function(x) min(x$pH, na.rm = T) )
+names(minpH)[2] <- 'minpH'
+CarbonateStage <- ddply( udat, .(id), function(x) max(x$CarbonateStage, na.rm = T) )
+names(CarbonateStage)[2] <- 'CarbonateStage'
+BioticCrustClass <- ddply( udat, .(id), function(x) max(x$BioticCrustClass, na.rm = T) )
+names(BioticCrustClass)[2] <- 'BioticCrustClass'
+
+
+#Factor
+maxDryValue <- ddply(.udata = udat, .(id), function(x) max(x$DryValue, na.rm = T))
+names(maxDryValue)[2] <- 'maxDryValue'
+minDryValue <- ddply(.udata = udat, .(id), function(x) min(x$DryValue, na.rm = T))
+names(minDryValue)[2] <- 'minDryValue'
+maxDryChroma <- ddply(.udata = udat, .(id), function(x) max(x$DryChroma, na.rm = T))
+names(maxDryChroma)[2] <- 'maxDryChroma'
+minDryChroma <- ddply(.udata = udat, .(id), function(x) min(x$DryChroma, na.rm = T))
+names(minDryChroma)[2] <- 'minDryChroma'
+maxMoistValue <- ddply(.udata = udat, .(id), function(x) max(x$MoistValue, na.rm = T))
+names(maxMoistValue)[2] <- 'maxMoistValue'
+minMoistValue <- ddply(.udata = udat, .(id), function(x) min(x$MoistValue, na.rm = T))
+names(minMoistValue)[2] <- 'minMoistValue'
+maxMoistChroma <- ddply(.udata = udat, .(id), function(x) max(x$MoistChroma, na.rm = T))
+names(maxMoistChroma)[2] <- 'maxMoistChroma'
+minMoistChroma <- ddply(.udata = udat, .(id), function(x) min(x$MoistChroma, na.rm = T))
+names(minMoistChroma)[2] <- 'minMoistChroma'
+
+# Change moist Chroma to 1st horizon and 2nd horizon as a proxy
+# for organic carbon accumulation in surface and subsurface horzons
+# A lower chroma means more organic carbon. Grassland soils have
+# higher fine root turnover.
+
+# Surface
+udat$Surface <- as.numeric(udat$top<1) # Turns surface horizon into 1, and all other into 0
+
+max.func.sur <- function(udat) {
+  max.moist.sur <- max(udat$Surface)
+  
+  return(udata.frame(Surface = udat$MoistChroma[udat$Surface==max.moist.sur]))
+}
+
+Surface <- ddply(udat, .(id), max.func.sur)
+names(Surface)[2] <- 'Surface'
+
+# Subsurface
+udat <- getanID(udata = udat, id.vars = "id") # Creates an ordered list of each horizon in a plot
+udat$Subsurface <- as.numeric(udat$.id==2) # Makes subsurface horizon 1, and all other horizons 0
+udat$MoistChroma[is.na(udat$MoistChroma)] <- 0
+
+max.func.sub <- function(udat) {
+  max.moist.sub <- max(udat$Subsurface)
+  
+  return(udata.frame(Subsurface = udat$MoistChroma[udat$Subsurface==max.moist.sub]))
+}
+
+Subsurface <- ddply(udat, .(id), max.func.sub)
+names(Subsurface)[2] <- 'Subsurface'
+
+#Numeric udata
+udat$top <- as.numeric(udat$top)
+udat$bottom <- as.numeric(udat$bottom)
+udat$ClayPercent <- as.numeric(udat$ClayPercent)
+udat$Elevation <- as.numeric(udat$Elevation)
+udat$SandPercent <- as.numeric(udat$SandPercent)
+udat$pH <- as.numeric(udat$pH)
+udat$Slope <- as.numeric(udat$Slope)
+udat$AWHC <- as.numeric(udat$AWHC)
+
+# Factor udata
+udat$DryHue <- as.factor(udat$DryHue)
+udat$DryValue <- as.factor(udat$DryValue)
+udat$DryChroma <- as.factor(udat$DryChroma)
+udat$MoistHue <- as.factor(udat$MoistHue)
+udat$MoistValue <- as.factor(udat$MoistValue)
+udat$MoistChroma <- as.factor(udat$MoistChroma)
+udat$Texture <- as.factor(udat$Texture)
+udat$SandSize <- as.factor(udat$SandSize)
+udat$Effervescence <- as.factor(udat$Effervescence)
+udat$CarbonateStage <- as.factor(udat$CarbonateStage)
+udat$BioticCrustClass <- as.factor(udat$BioticCrustClass)
+udat$HzNum <- as.factor(udat$HzNum)
+udat$Aspect <- as.factor(udat$Aspect)
+
+
+all <- join(Elevation, SlopeShape, by = 'id', type = 'inner')
+all <- join(all, Slope, by = 'id', type = 'inner')
+all <- join(all, maxClay, by = 'id', type = 'inner')
+all <- join(all, minClay, by = 'id', type = 'inner')
+all <- join(all, maxSand, by = 'id', type = 'inner')
+all <- join(all, minSand, by = 'id', type = 'inner')
+all <- join(all, maxDepth, by = 'id', type = 'inner')
+all <- join(all, maxpH, by = 'id', type = 'inner')
+all <- join(all, minpH, by = 'id', type = 'inner')
+all <- join(all, maxDryValue, by = 'id', type = 'inner')
+all <- join(all, minDryValue, by = 'id', type = 'inner')
+all <- join(all, maxDryChroma, by = 'id', type = 'inner')
+all <- join(all, minDryChroma, by = 'id', type = 'inner')
+all <- join(all, maxMoistValue, by = 'id', type = 'inner')
+all <- join(all, minMoistValue, by = 'id', type = 'inner')
+all <- join(all, maxMoistChroma, by = 'id', type = 'inner')
+all <- join(all, minMoistChroma, by = 'id', type = 'inner')
+all <- join(all, CarbonateStage, by = 'id', type = 'inner')
+all <- join(all, BioticCrustClass, by = 'id', type = 'inner')
+all <- join(all, Surface, by = 'id', type = 'inner')
+all <- join(all, Subsurface, by = 'id', type = 'inner')
+
+
+# Create new soil parameter where depth is binary.
+# if the maximum depth is >50/100/150/200 then 1, if not then 0
+all$Depth50 <- as.numeric(all$maxDepth > 50)
+all$Depth100 <- as.numeric(all$maxDepth > 100)
+all$Depth150 <- as.numeric(all$maxDepth > 150)
+all$Depth200 <- as.numeric(all$maxDepth == 200)
+
+#Now calculate depth weighted averages of each continuous variable, then append these to the other variables. 
+#Convert to SoilProfileCollection
+depths(udat) <- id ~ top + bottom
+
+# within each profile, compute weighted means, over the intervals: 0-25,0-50,0-100, removing NA if present 
+d25 <- slab(udat, id ~ AWHC, slab.structure = c(0,25), slab.fun = mean, na.rm=TRUE)
+d50 <- slab(udat, id ~ AWHC, slab.structure = c(0,50), slab.fun = mean, na.rm=TRUE)
+d100 <- slab(udat, id ~ AWHC, slab.structure = c(0,100), slab.fun = mean, na.rm=TRUE)
+
+# reshape to wide format, remove unneeded variables and rename. 
+AWC25 <- dcast(d25, id + top + bottom ~ variable, value.var = 'value')
+AWC25 <- AWC25[,-c(2,3)]
+names(AWC25)[2] <- 'AWC25'
+
+AWC50 <- dcast(d50, id + top + bottom ~ variable, value.var = 'value')
+AWC50 <- AWC50[,-c(2,3)]
+names(AWC50)[2] <- 'AWC50'
+
+AWC100 <- dcast(d100, id + top + bottom ~ variable, value.var = 'value')
+AWC100 <- AWC100[,-c(2,3)]
+names(AWC100)[2] <- 'AWC100'
+
+all <- join(all, AWC25, by = 'id', type = 'inner')
+all <- join(all, AWC50, by = 'id', type = 'inner')
+all <- join(all, AWC100, by = 'id', type = 'inner')
+all <- join(all, MaxAWC, by = 'id', type = 'inner')
+all <- join(all, TotalAWC, by = 'id', type = 'inner')
+
+write.csv(all,file="F:/Soils/SoilEnvironmentaludata.csv", row.names=FALSE)

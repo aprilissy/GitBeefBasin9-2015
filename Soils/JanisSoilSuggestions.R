23698a2b16629fd7ff911ce9dbc2c89a06cea588
April <april.darger@aggiemail.usu.edu>
2015-09-22 12:09:27 -0600

    Changed dry/moist Hue to Redness Scale, effervescence to scale, modified how I got H1

--- Soils/JanisSoilSuggestions.R
+++ Soils/JanisSoilSuggestions.R
@@ -18,13 +18,58 @@ library(plyr)
 dat <- read.csv("F:/Soils/SoilDataFitUSGSColumns.csv", header = T,nrows = 444)
 dat <- getanID(data = dat, id.vars = "id") # Creates an ordered list of each horizon in a plot
 
+# unique(c(as.character(dat$MoistHue)))
+
+  # Scale Hue - Redness Scale - Degree of Redness
+  # I have 4 Hue values:2.5YR, 5YR, 7.5YR, and 10YR
+  # They will be numbered from least to most red. 2.5YR=4, 5YR=3, 7.5YR=2, 10YR=1.
+{
+dat$DryHue <- sub("2.5YR", "4", dat$DryHue, ignore.case = FALSE)
+dat$DryHue <- sub("7.5YR", "2", dat$DryHue, ignore.case = FALSE)
+dat$DryHue <- sub("5YR", "3", dat$DryHue, ignore.case = FALSE)
+dat$DryHue <- sub("10YR", "1", dat$DryHue, ignore.case = FALSE)
+dat$DryHue <- as.factor(dat$DryHue)
+
+dat$MoistHue <- sub("2.5YR", "4", dat$MoistHue, ignore.case = FALSE)
+dat$MoistHue <- sub("7.5YR", "2", dat$MoistHue, ignore.case = FALSE)
+dat$MoistHue <- sub("5YR", "3", dat$MoistHue, ignore.case = FALSE)
+dat$MoistHue <- sub("10YR", "1", dat$MoistHue, ignore.case = FALSE)
+dat$MoistHue <- as.factor(dat$MoistHue)
+
+dat$Effervescence <- sub("VE", "4", dat$Effervescence, ignore.case = FALSE)
+dat$Effervescence <- sub("ST", "3", dat$Effervescence, ignore.case = FALSE)
+dat$Effervescence <- sub("SL", "2", dat$Effervescence, ignore.case = FALSE)
+dat$Effervescence <- sub("VS", "1", dat$Effervescence, ignore.case = FALSE)
+dat$Effervescence <- sub("NE", "0", dat$Effervescence, ignore.case = FALSE)
+dat$Effervescence <- sub("LS", "2", dat$Effervescence, ignore.case = FALSE)
+dat$Effervescence <- as.factor(dat$Effervescence)
+
+}
+
 H1 <- dat[ which(dat$.id=='1'), ] # Pull out horizon #1
 H1 <- subset(H1, select = -c(Horizon,top,.id, HzNum) ) # Remove top, Horizon, HzNum, .id
 # Pull out data that is not only for horizon 1, then take it out of H1
 Plot <- subset(H1, select = c(id,Elevation,Aspect,Slope,SlopeShape,CarbonateStage,BioticCrustClass))
 H1 <- subset(H1, select = -c(Elevation,Aspect,Slope,SlopeShape,CarbonateStage,BioticCrustClass))
+
 colnames(H1) = paste("H1_", colnames(H1)) # Rename variables for H1
 rename(H1, c("H1_ id"="id", "H1_ bottom"="H1_ Depth")) 
+
+{Plot$SlopeShape <- sub("LC", "CL", Plot$SlopeShape, ignore.case = FALSE)
+Plot$SlopeShape <- sub("VC", "CV", Plot$SlopeShape, ignore.case = FALSE)
+Plot$SlopeShape <- sub("VL", "LV", Plot$SlopeShape, ignore.case = FALSE)
+Plot$SlopeShape <- as.factor(Plot$SlopeShape)}
+# unique(c(as.character(Plot$SlopeShape)))
+
+dat <- subset(dat, select = -c(Horizon,Theta_fc,Theta_pwp, HzNum,SlopeShape) )
+
+
+
+
+
+
+
+
 
 
 

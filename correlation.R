7733cb50f75812d174d636b668826a73fa648753
April <april.darger@aggiemail.usu.edu>
2015-10-22 14:37:57 -0600

    Ran through all Elevation correlations.

--- correlation.R
+++ correlation.R
@@ -1,8 +1,20 @@
-rel<-read.csv("F:/LPI/AprilLPIRelativeCoverCommonInExcel.csv",header=TRUE, row.names=1)
-den<-read.csv("F:/LPI/AprilLPIplotXspp.csv",header=TRUE, row.names=1)
-den <- den/150
-write.csv(den,file="F:/LPI/AprilLPIDenM2.csv", row.names=TRUE)
+rel<-read.csv("F:/LPI/Output/AprilLPIRelativeCover.csv",header=TRUE, row.names=1)
+den<-read.csv("F:/LPI/Output/AprilLPIDensityM2.csv",header=TRUE, row.names=1)
 
+
+reg1 <- lm(write~read)
+par(cex=.8)
+plot(read, write)
+abline(reg1)
+
+reg1 <- lm(rel$ARTR2~den$ARTR2)
+par(cex=.8)
+plot(rel$ARTR2,den$ARTR2)
+abline(reg1)
+
+# Basic Scatterplot Matrix
+pairs(~ARTR2+ARTR2.D+ATCA2+ATCA2.D+KRLA2+BOGR2+BOGR2.D,data=rel, 
+      main="Simple Scatterplot Matrix")
 
 plot(rel$ARTR2, rel$SPCR, col='navyblue')
 plot(den$ARTR2, den$SPCR, col='darkgreen')
@@ -10,14 +22,14 @@ plot(den$ARTR2, den$SPCR, col='darkgreen')
 plot(rel$SPCR)
 plot(den$SPCR)
 
-plot(rel$BOGR, rel$SPCR, col='navyblue')
-plot(den$BOGR, den$SPCR, col='darkgreen')
+plot(rel$BOGR2, rel$SPCR, col='navyblue')
+plot(den$BOGR2, den$SPCR, col='darkgreen')
 
-plot(rel$ARTR2, rel$BOGR, col='navyblue')
-plot(den$ARTR2, den$BOGR, col='darkgreen')
+plot(rel$ARTR2, rel$BOGR2, col='navyblue')
+plot(den$ARTR2, den$BOGR2, col='darkgreen')
 
-plot(rel$BOGR)
-plot(den$BOGR)
+plot(rel$BOGR2)
+plot(den$BOGR2)
 
 
 plot(rel$ARTR2, rel$KRLA2, col='navyblue')
@@ -65,8 +77,8 @@ points(rel$ARTR2, den$ARTR2, col='green')
 points(rel$ARTR2.D, den$ARTR2.D, col='blue')
 points(rel$ATCA2, den$ATCA2, col='green4')
 points(rel$ATCA2.D, den$ATCA2.D, col='orange')
-points(rel$BOGR, den$BOGR, col='navyblue')
-points(rel$BOGR.D, den$BOGR.D, col='red')
+points(rel$BOGR2, den$BOGR2, col='navyblue')
+points(rel$BOGR2.D, den$BOGR2.D, col='red')
 
 
 
@@ -75,9 +87,170 @@ u<-read.csv("F:/Soils/SoilEnvironmentaldataUSGSApril.csv",header=TRUE, row.names
 p<-read.csv("F:/Soils/SoilEnvironmentaldataNSplain.csv",header=TRUE, row.names=1)
 a<-read.csv("F:/Soils/SoilEnvironmentaldataApril.csv",header=TRUE, row.names=1)
 
+
+
+
+# Basic Scatterplot Matrix
+pairs(~Elevation+Aspect+SlopeShape+Slope,data=u, 
+      main="Simple Scatterplot Matrix")
+
+
+plot(u$Elevation~u$Aspect, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Aspect), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Slope, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Slope), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$CarbonateStage, col='blue', main='USGS')
+abline(lm(u$Elevation~u$CarbonateStage), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$BioticCrustClass, col='blue', main='USGS')
+abline(lm(u$Elevation~u$BioticCrustClass), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$PedonDepth, col='blue', main='USGS')
+abline(lm(u$Elevation~u$PedonDepth), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Depth200, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Depth200), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.DryRed, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.DryRed), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.DryValue, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.DryValue), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.DryCClass, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.DryCClass), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.MoistRed, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.MoistRed), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.MoistValue, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.MoistValue), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.MoistCClass, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.MoistCClass), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.SandPercent, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.SandPercent), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.ClayPercent, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.ClayPercent), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.pH, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.pH), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.EfferScale, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.EfferScale), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.Depth, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.Depth), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.DWA_AWC, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.DWA_AWC), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Tot.DryRed, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Tot.DryRed), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Tot.DryValue, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Tot.DryValue), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Tot.DryCClass, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Tot.DryCClass), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Tot.MoistRed, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Tot.MoistRed), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Tot.MoistValue, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Tot.MoistValue), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Tot.MoistCClass, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Tot.MoistCClass), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Tot.EfferScale, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Tot.EfferScale), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$DWAClay, col='blue', main='USGS')
+abline(lm(u$Elevation~u$DWAClay), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$DWASand, col='blue', main='USGS')
+abline(lm(u$Elevation~u$DWASand), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$DWApH, col='blue', main='USGS')
+abline(lm(u$Elevation~u$DWApH), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$DWA.AWHC, col='blue', main='USGS')
+abline(lm(u$Elevation~u$DWA.AWHC), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Sand.50, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Sand.50), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Clay.50, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Clay.50), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$pH.50, col='blue', main='USGS')
+abline(lm(u$Elevation~u$pH.50), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$DryValue.50, col='blue', main='USGS')
+abline(lm(u$Elevation~u$DryValue.50), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$EfferScale.50, col='blue', main='USGS')
+abline(lm(u$Elevation~u$EfferScale.50), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$AWHC.50, col='blue', main='USGS')
+abline(lm(u$Elevation~u$AWHC.50), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$MaxSand, col='blue', main='USGS')
+abline(lm(u$Elevation~u$MaxSand), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$MaxClay, col='blue', main='USGS')
+abline(lm(u$Elevation~u$MaxClay), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$MaxpH, col='blue', main='USGS')
+abline(lm(u$Elevation~u$MaxpH), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$MaxDryValue, col='blue', main='USGS')
+abline(lm(u$Elevation~u$MaxDryValue), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$MaxEffervescence, col='blue', main='USGS')
+abline(lm(u$Elevation~u$MaxEffervescence), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$MaxAWHC, col='blue', main='USGS')
+abline(lm(u$Elevation~u$MaxAWHC), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$SlopeShape, col='blue', main='USGS')
+abline(lm(u$Elevation~u$SlopeShape), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$DepthClass, col='blue', main='USGS')
+abline(lm(u$Elevation~u$DepthClass), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.Texture, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.Texture), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$H1.SandSize, col='blue', main='USGS')
+abline(lm(u$Elevation~u$H1.SandSize), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Tot.Texture, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Tot.Texture), col="red") # regression line (y~x) 
+
+plot(u$Elevation~u$Tot.SandSize, col='blue', main='USGS')
+abline(lm(u$Elevation~u$Tot.SandSize), col="red") # regression line (y~x) 
+
+
+
+
+
+
+
+
 plot(u$MaxClay, u$Clay.50, col='blue', main='USGS')
+abline(lm(u$MaxClay~u$Clay.50), col="red") # regression line (y~x) 
+
 plot(p$MaxClay, p$Clay.50, col='blue', main='Plains')
+abline(lm(p$MaxClay~p$Clay.50), col="red") # regression line (y~x) 
+
 plot(a$MaxClay, a$Clay.50, col='blue', main='April')
+abline(lm(a$MaxClay~a$Clay.50), col="red") # regression line (y~x) 
 
 plot(u$MaxSand, u$Sand.50, col='blue', main='USGS')
 plot(p$MaxSand, p$Sand.50, col='blue', main='Plains')
